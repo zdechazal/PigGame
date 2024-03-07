93c88573b451e0df7a1a59c5e1ec2e71050b1790
Zaskia de Chazal <zaskia.de_chazal0001@stud.hkr.se>
2024-03-07 20:14:41 +0100

    comments

--- PigGame/Computer.py
+++ PigGame/Computer.py
@@ -1,4 +1,3 @@
-
 import time
 
 class Computer:
--- PigGame/Game.py
+++ PigGame/Game.py
@@ -3,6 +3,7 @@ from Human import Human
 from Computer import Computer
 
 class Game:
+    winning_score = 100
     def __init__(self, player_one, player_two):
         self.player_one = player_one
         self.player_two = player_two
@@ -11,9 +12,9 @@ class Game:
     def start_game(self):
         player = self.player_one
         while True:
-            turn = Turn(player)
-            turn_result = turn.play_turn()
-            if turn_result == -1:
+            turn = Turn(player) 
+            turn_result = turn.play_turn() #starts a player's turn
+            if turn_result == -1: #player quit the game
                 self.reset_game_score()
                 break
             player.game_score += turn_result
@@ -22,7 +23,7 @@ class Game:
                 self.add_user_stats(player)
                 self.reset_game_score()
                 break
-            player = self.switch_player(player)     
+            player = self.switch_player(player)
             
 
     def switch_player(self, player):
@@ -33,7 +34,7 @@ class Game:
 
     
     def has_won(self, player):
-        if player.game_score >= 15:
+        if player.game_score >= Game.winning_score:
            print(f"\n{player.username} has won!!!")
            return True
         return False
@@ -56,9 +57,3 @@ class Game:
     def reset_game_score(self):
         self.player_one.game_score = 0
         self.player_two.game_score = 0
-
-        
-    
-    
-
-    
