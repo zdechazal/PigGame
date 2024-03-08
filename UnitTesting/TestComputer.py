68a0e7dfc3ea1e3823d39d7ab2eab06ab9a7bd58
Daniel Holderik <danielholderik@gmail.com>
2024-03-08 10:52:52 +0100

    .

--- UnitTesting/TestComputer.py
+++ UnitTesting/TestComputer.py
@@ -1,7 +1,7 @@
 import unittest
 
 
-class MyTestCase(unittest.TestCase):
+class test_computer(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
