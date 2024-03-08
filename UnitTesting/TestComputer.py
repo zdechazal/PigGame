413b5d74aae8a1c384a95f49125d99ab1dc1bfe0
Sergej <sergej.macut0023@stud.hkr.se>
2024-03-08 15:36:18 +0100

    Added two more test cases

--- UnitTesting/TestComputer.py
+++ UnitTesting/TestComputer.py
@@ -2,35 +2,48 @@ import unittest
 from unittest.mock import patch
 from Computer import Computer
 
+
 class TestComputer(unittest.TestCase):
     def setUp(self):
-        self.computer_easy = Computer("1")
-        self.computer_medium = Computer("2")
-        self.computer_hard = Computer("3")
+        self.computer = Computer("1")
+        self.easy = Computer("1")
+        self.medium = Computer("2")
+        self.hard = Computer("3")
 
     def test_is_rolling_easy(self):
         with patch('builtins.input', side_effect=['n']):
-            self.computer_easy.turn_total = 10
-            self.assertTrue(self.computer_easy.is_rolling())
+            self.easy.turn_total = 10
+            self.assertTrue(self.easy.is_rolling())
 
-            self.computer_easy.turn_total = 25
-            self.assertFalse(self.computer_easy.is_rolling())
+            self.easy.turn_total = 25
+            self.assertFalse(self.easy.is_rolling())
 
     def test_is_rolling_medium(self):
         with patch('builtins.input', side_effect=['n']):
-            self.computer_medium.turn_total = 20
-            self.assertTrue(self.computer_medium.is_rolling())
+            self.medium.turn_total = 20
+            self.assertTrue(self.medium.is_rolling())
 
-            self.computer_medium.turn_total = 30
-            self.assertFalse(self.computer_medium.is_rolling())
+            self.medium.turn_total = 30
+            self.assertFalse(self.medium.is_rolling())
 
     def test_is_rolling_hard(self):
         with patch('builtins.input', side_effect=['n']):
-            self.computer_hard.turn_total = 10
-            self.assertTrue(self.computer_hard.is_rolling())
+            self.hard.turn_total = 10
+            self.assertTrue(self.hard.is_rolling())
 
-            self.computer_hard.turn_total = 20
-            self.assertFalse(self.computer_hard.is_rolling())
+            self.hard.turn_total = 20
+            self.assertFalse(self.hard.is_rolling())
+
+    def test_decision_logic_below_max(self):
+        # Test when turn_total is below turn_max
+        self.computer.turn_total = 10
+        self.assertTrue(self.computer.decision_logic(20))
+
+    def test_decision_logic_above_max(self):
+        # Test when turn_total is above turn_max
+        self.computer.turn_total = 25
+        self.assertFalse(self.computer.decision_logic(20))
+
 
 if __name__ == '__main__':
     unittest.main()
